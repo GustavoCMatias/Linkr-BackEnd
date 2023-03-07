290ad7404f85c2ab9edaefb8e3d5b68937985dc3
GustavoCMatias <gustavocmatias@gmail.com>
2023-03-07 17:35:52 -0300

    feat: rota get by hashtag construída

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors"
+import hashtagRouter from "./router/hashtag.router";
 
 const server = express()
 server.use(express.json())
 server.use(cors())
-server.use([])
+server.use([hashtagRouter])
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Server running in port: ${port}`));
---
+++ src/middleware/authValidation.middleware.js
@@ -0,0 +1,20 @@
+import { searchToken } from "../repository/auth.repository.js"
+
+
+export default async function tokenValidation(req, res, next){
+    const {authorization} = req.headers
+    const token = authorization?.replace("Bearer ", "")
+
+    if(!token) return res.sendStatus(401)
+
+    try{
+        const row_info = searchToken(token)
+
+        if(row_info[1] === 0) return res.sendStatus(401)
+        res.locals.user = row_info[0][0].user_id
+        next()
+    }catch(error){
+        res.status(500).send(error.message)
+    }
+
+}
