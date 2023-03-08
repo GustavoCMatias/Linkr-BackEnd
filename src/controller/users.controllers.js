b4b55151ca7b4bbca60ce6553cb1f9df4ac55c8f
Rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2023-03-08 17:03:26 -0300

    feat: return user

--- src/controller/users.controllers.js
+++ src/controller/users.controllers.js
@@ -1,4 +1,6 @@
 import { db } from "../database/database.connection.js";
+import { searchUser } from "../repository/user.repository.js";
+
 
 export async function postRegister(req, res) {
     const { email, cryptoPassword, username, picture_url } = res.locals.register;
@@ -16,7 +18,21 @@ export async function postSignIn(req, res) {
     try {
         await db.query('INSERT INTO sessions (user_id, token) VALUES ($1, $2);', [userId, token]);
 
-        return res.status(200).send({ token });
+
+        return res.status(200).send({ token })
+    } catch (error) {
+        return res.status(500).send("server error: " + error);
+    }
+}
+
+export async function returnUser(req, res) {
+    const user_id  = res.locals.user;
+    console.log(res.locals.user)
+
+    try {
+        const user = await searchUser(user_id);
+
+        return res.status(200).send(user[0][0] );
     } catch (error) {
         return res.status(500).send("server error: " + error);
     }
--- src/router/users.router.js
+++ src/router/users.router.js
@@ -1,13 +1,15 @@
-import { postRegister, postSignIn } from "../controller/users.controllers.js";
+import { postRegister, postSignIn, returnUser } from "../controller/users.controllers.js";
 import { registerVerifications, signInVerifications } from "../middleware/user.middleware.js";
 import { validateSchema } from "../middleware/validate.schema.js";
 import { register, signIn } from "../schema/user.schema.js";
 import { Router } from "express";
+import tokenValidation from "../middleware/authValidation.middleware.js";
 
 
 const usersRouter = Router();
 
 usersRouter.post('/sign-up', validateSchema(register), registerVerifications, postRegister);
 usersRouter.post('/sign-in', validateSchema(signIn), signInVerifications, postSignIn);
+usersRouter.get('/return-user', tokenValidation, returnUser)
 
 export default usersRouter;
