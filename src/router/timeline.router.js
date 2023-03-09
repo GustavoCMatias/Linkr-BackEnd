d839c34acaf98b5cdeb88305f46497b5ae93ea98
GersonGoncalves <gerson.devpro@gmail.com>
2023-03-09 15:29:23 -0300

    CRUD Post

--- src/router/timeline.router.js
+++ src/router/timeline.router.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import { createPost, getTimeline } from "../controller/timeline.controller.js";
-import { validateCreatePost } from "../middleware/timeline.middleware.js";
+import { createPost, deletePost, getTimeline, updatePost } from "../controller/timeline.controller.js";
+import { deleteLikesPost, validateCreatePost, validateDeleteOrPut, validateToken } from "../middleware/timeline.middleware.js";
 
 const feed = Router()
 
-feed.post('/timeline', validateCreatePost, createPost)
+feed.post('/timeline', validateToken, validateCreatePost, createPost)
 feed.get('/timeline', getTimeline)
-// Ver com o time se preciso de 2 rotas (get e post), visto que a página é tanto 'get' como 'post' (pensando na validação do token)
+feed.put('/timeline/:id', validateToken, validateDeleteOrPut, validateCreatePost, updatePost)
+feed.delete('/timeline/:id', validateToken, validateDeleteOrPut, deleteLikesPost, deletePost)
 
 export {
     feed
