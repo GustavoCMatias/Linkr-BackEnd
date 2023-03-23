b9fbff6286764284b353225bc010f5094afd0bdc
GustavoCMatias <gustavocmatias@gmail.com>
2023-03-23 10:21:05 -0300

    Rota post comment completa

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -5,9 +5,9 @@ export async function postComment(req, res){
     const {content, post_id} = req.body
     const user_id = res.locals.user
 
-    try{
-        res.status(200).send(render);
+    try{       
         await insertNewComment(content, post_id, user_id)
+        res.status(201).send();
     }catch (error) {
         res.status(500).send(error.message)
     }
